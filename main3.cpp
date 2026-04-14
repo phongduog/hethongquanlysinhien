23f9c822b698a123a37f22206c8e83f8e15cc0e9
phongduog <phongtuoi12@gmail.com>
2026-04-14 17:47:51 +0700

    Update main3.cpp

--- main3.cpp
+++ main3.cpp
@@ -14,7 +14,6 @@ struct SinhVien {
     int tuoi;
     char khoaHoc[DO_DAI_KHOA_HOC_TOI_DA];
 };
-
 SinhVien danhSachSinhVien[TONG_SINH_VIEN_TOI_DA];
 int soLuongHienTai = 0;
 
@@ -37,47 +36,48 @@ bool laMaSoDuyNhat(int maSo) {
 
 void themSinhVien() {
     if (soLuongHienTai >= TONG_SINH_VIEN_TOI_DA) {
-        ("Danh sach sinh vien da day.", 100);
+        inCanGiua("Danh sach sinh vien da day.", 100);
         return;
     }
     int maSo;
-    ("Nhap Ma SV: ", 100);
+    inCanGiua("Nhap Ma SV: ", 100);
     cin >> maSo;
     while (!laMaSoDuyNhat(maSo)) {
-        ("Ma SV da ton tai. Vui long nhap lai: ", 100);
+        inCanGiua("Ma SV da ton tai. Vui long nhap lai: ", 100);
         cin >> maSo;
     }
     danhSachSinhVien[soLuongHienTai].maSo = maSo;
 
     cin.ignore();
-    ("Nhap Ho Ten: ", 100);
+
+    inCanGiua("Nhap Ho Ten: ", 100);
     cin.getline(danhSachSinhVien[soLuongHienTai].hoTen, DO_DAI_TEN_TOI_DA);
 
-    ("Nhap Tuoi: ", 100);
+    inCanGiua("Nhap Tuoi: ", 100);
     cin >> danhSachSinhVien[soLuongHienTai].tuoi;
 
     while (cin.fail() || danhSachSinhVien[soLuongHienTai].tuoi <= 0 || danhSachSinhVien[soLuongHienTai].tuoi > 100) {
         cin.clear();
         cin.ignore(1000, '\n');
-        ("Tuoi khong hop le. Vui long nhap lai (1-100): ", 100);
+        inCanGiua("Tuoi khong hop le. Vui long nhap lai (1-100): ", 100);
         cin >> danhSachSinhVien[soLuongHienTai].tuoi;
     }
     cin.ignore();
 
-    ("Nhap Khoa hoc: ", 100);
+    inCanGiua("Nhap Khoa hoc: ", 100);
     cin.getline(danhSachSinhVien[soLuongHienTai].khoaHoc, DO_DAI_KHOA_HOC_TOI_DA);
 
     soLuongHienTai++;
-    ("Them sinh vien thanh cong!", 100);
+    inCanGiua("Them sinh vien thanh cong!", 100);
 }
 
 void xemTatCaSinhVien() {
     if (soLuongHienTai == 0) {
-        ("Chua co du lieu sinh vien.", 100);
+        inCanGiua("Chua co du lieu sinh vien.", 100);
         return;
     }
-    ("MSV\tHo Ten\t\tTuoi\tKhoa Hoc", 100);
-    ("-------------------------------------------", 100);
+    inCanGiua("MSV\tHo Ten\t\tTuoi\tKhoa Hoc", 100);
+    inCanGiua("-------------------------------------------", 100);
     for (int i = 0; i < soLuongHienTai; i++) {
         cout << setw(25) << setfill(' ') << danhSachSinhVien[i].maSo << "\t"
              << setw(25) << setfill(' ') << danhSachSinhVien[i].hoTen << "\t\t"
@@ -88,7 +88,7 @@ void xemTatCaSinhVien() {
 
 void timKiemTheoMaSo() {
     int maSo;
-    ("Nhap MSV can tim: ", 100);
+    inCanGiua("Nhap MSV can tim: ", 100);
     cin >> maSo;
     for (int i = 0; i < soLuongHienTai; i++) {
         if (danhSachSinhVien[i].maSo == maSo) {
@@ -97,12 +97,12 @@ void timKiemTheoMaSo() {
             return;
         }
     }
-    ("Khong tim thay sinh vien.", 100);
+    inCanGiua("Khong tim thay sinh vien.", 100);
 }
 
 void timKiemTheoTen() {
     char tenCanTim[DO_DAI_TEN_TOI_DA];
-    ("Nhap Ten can tim: ", 50);
+    inCanGiua("Nhap Ten can tim: ", 50);
     cin.ignore();
     cin.getline(tenCanTim, DO_DAI_TEN_TOI_DA);
     for (int i = 0; i < soLuongHienTai; i++) {
@@ -112,16 +112,16 @@ void timKiemTheoTen() {
             return;
         }
     }
-    ("Khong tim thay sinh vien.", 100);
+    inCanGiua("Khong tim thay sinh vien.", 100);
 }
 
 void xoaSinhVien() {
     int maSo;
-    ("Nhap MSV can xoa: ", 100);
+    inCanGiua("Nhap MSV can xoa: ", 100);
     cin >> maSo;
     for (int i = 0; i < soLuongHienTai; i++) {
         if (danhSachSinhVien[i].maSo == maSo) {
-            ("Ban co chac muon xoa? (y/n): ", 100);
+            inCanGiua("Ban co chac muon xoa? (y/n): ", 100);
             char xacNhan;
             cin >> xacNhan;
             if (xacNhan == 'y' || xacNhan == 'Y') {
@@ -129,35 +129,35 @@ void xoaSinhVien() {
                     danhSachSinhVien[j] = danhSachSinhVien[j + 1];
                 }
                 soLuongHienTai--;
-                ("Xoa thanh cong.", 100);
+                inCanGiua("Xoa thanh cong.", 100);
             } else {
-                ("Da huy xoa.", 100);
+                inCanGiua("Da huy xoa.", 100);
             }
             return;
         }
     }
-    ("Khong tim thay MSV.", 100);
+    inCanGiua("Khong tim thay MSV.", 100);
 }
 
 void capNhatSinhVien() {
     int maSo;
-    ("Nhap MSV can cap nhat: ", 100);
+    inCanGiua("Nhap MSV can cap nhat: ", 100);
     cin >> maSo;
     for (int i = 0; i < soLuongHienTai; i++) {
         if (danhSachSinhVien[i].maSo == maSo) {
             cin.ignore();
-            ("Nhap Ten moi: ", 100);
+            inCanGiua("Nhap Ten moi: ", 100);
             cin.getline(danhSachSinhVien[i].hoTen, DO_DAI_TEN_TOI_DA);
-            ("Nhap Tuoi moi: ", 100);
+            inCanGiua("Nhap Tuoi moi: ", 100);
             cin >> danhSachSinhVien[i].tuoi;
             cin.ignore();
-            ("Nhap Khoa hoc moi: ", 100);
+            inCanGiua("Nhap Khoa hoc moi: ", 100);
             cin.getline(danhSachSinhVien[i].khoaHoc, DO_DAI_KHOA_HOC_TOI_DA);
-            ("Cap nhat thanh cong!", 100);
+            inCanGiua("Cap nhat thanh cong!", 100);
             return;
         }
     }
-    ("Khong tim thay MSV.", 100);
+    inCanGiua("Khong tim thay MSV.", 100);
 }
 
 void sapXepTheoTen() {
@@ -170,12 +170,12 @@ void sapXepTheoTen() {
             }
         }
     }
-    ("Da sap xep danh sach theo ten.", 100);
+    inCanGiua("Da sap xep danh sach theo ten.", 100);
 }
 
 void thongKe() {
     if (soLuongHienTai == 0) {
-        ("Khong co du lieu de thong ke.", 100);
+        inCanGiua("Khong co du lieu de thong ke.", 100);
         return;
     }
     int tongTuoi = 0;
@@ -189,18 +189,18 @@ void thongKe() {
 int main() {
     int luaChon;
     do {
-        ("-------- HE THONG QUAN LY SINH VIEN --------", 100);
-        ("1. Them sinh vien", 100);
-        ("2. Xem tat ca sinh vien", 100);
-        ("3. Tim kiem theo MSV", 100);
-        ("4. Tim kiem theo Ten", 100);
-        ("5. Xoa sinh vien", 100);
-        ("6. Cap nhat sinh vien", 100);
-        ("7. Sap xep theo ten", 100);
-        ("8. Thong ke", 100);
-        ("9. Thoat", 100);
-        ("--------------------------------------------", 100);
-        ("Chon chuc nang: ", 100);
+        inCanGiua("-------- HE THONG QUAN LY SINH VIEN --------", 100);
+        inCanGiua("1. Them sinh vien", 100);
+        inCanGiua("2. Xem tat ca sinh vien", 100);
+        inCanGiua("3. Tim kiem theo MSV", 100);
+        inCanGiua("4. Tim kiem theo Ten", 100);
+        inCanGiua("5. Xoa sinh vien", 100);
+        inCanGiua("6. Cap nhat sinh vien", 100);
+        inCanGiua("7. Sap xep theo ten", 100);
+        inCanGiua("8. Thong ke", 100);
+        inCanGiua("9. Thoat", 100);
+        inCanGiua("--------------------------------------------", 100);
+        inCanGiua("Chon chuc nang: ", 100);
         cin >> luaChon;
 
         switch (luaChon) {
@@ -212,8 +212,8 @@ int main() {
             case 6: capNhatSinhVien(); break;
             case 7: sapXepTheoTen(); break;
             case 8: thongKe(); break;
-            case 9: ("Tam biet!", 50); break;
-            default: ("Lua chon khong hop le!", 50);
+            case 9: inCanGiua("Tam biet!", 50); break;
+            default: inCanGiua("Lua chon khong hop le!", 50);
         }
     } while (luaChon != 9);
     return 0;
